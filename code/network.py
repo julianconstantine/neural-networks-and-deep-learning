2aed4c981b85fb832eb5252049d211993f04ac8d
julianconstantine <julian.caracotsios@gmail.com>
2016-10-05 10:29:25 -0500

    finished reading ch 2

--- code/network.py
+++ code/network.py
@@ -82,9 +82,62 @@ class Network(object):
         self.weights = [w - (eta/len(mini_batch))*nw for w, nw in zip(self.weights, nabla_w)]
         self.biases = [b - (eta/len(mini_batch))*nb for b, nb in zip(self.biases, nabla_b)]
 
+    def backprop(self, x, y):
+        """Return a tuple (nabla_b, nabla_w) representing the gradient for the cost function C_x. nabla_b and
+        nabla_w are layer-by-layer lists of NumPy arryas, similar to self.biases and self.weights"""
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+
+        # FEEDFORWARD
+        activation = x
+
+        # List to store all activations, layer by layer
+        activations = [x]
+
+        # List to store all the weighted inputs, layer by layer
+        zs = []
+
+        for b, w in zip(self.biases, self.weights):
+            # Calculate weighted input
+            z = np.dot(w, activation) + b
+            zs.append(z)
+
+            # Calculation activation in next layer
+            activation = sigmoid(z)
+            activations.append(activation)
+
+        # BACKWARD PASS
+        delta = self.cost_derivative(activations[-1], y) * sigmoid_prime(zs[-1])
+        nabla_b[-1] = delta
+        nabla_w[-1] = np.dot(delta, activations[-2].transpose())
+
+        # Note that the variable l in the loop below is used a little differently than the notation in Chapter 2 of
+        # the book. Here, l = 1 means the last layer of neurons, l = 2 is the second-to-last layer, and so on. It's a
+        # renumbering of the scheme in the book, used here to take advantage of the fact that Python can use negative
+        # indices in lists
+        for l in range(2, self.num_layers):
+            z = zs[-l]
+            sp = sigmoid_prime(z)
+            delta = np.dot(self.weights[-l+1].transpose(), delta) * sp
+
+            nabla_b[-l] = delta
+            nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
+
+        return nabla_b, nabla_w
+
+    def cost_derivative(self, output_activations, y):
+        """Return the vector of partial derivatives of C_x with respect to the output activations"""
+        return output_activations - y
+
 
 def sigmoid(z):
+    """The sigmoid function"""
     return 1 / (1 + np.exp(-z))
+
+
+def sigmoid_prime(z):
+    """Derivative of the sigmoid function"""
+    return sigmoid(z) * (1 - sigmoid(z))
 
 
 net = Network([2, 3, 1])
